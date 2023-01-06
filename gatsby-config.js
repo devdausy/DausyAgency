926b938e5b61678089a469ac928fc68e23f7f46c
Silverback <jan.dausy@gmail.com>
2023-01-06 16:23:41 +0100

    deploy homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,19 +15,15 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-wordpress",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        /*
+         * De volledige URL van je Headless WordPress site's GraphQL API.
+         * Voorbeeld : "https://www.example-site.com/graphql"
+         */
+        url: "http://localhost:10003/graphql",
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,105 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import * as React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Artist from "../components/artist"
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  headerTitle,
+  CTA,
+  section,
+  subtitle,
+  artists,
+} from "../page.module.css"
 
-const IndexPage = () => {
+const IndexPage = ({
+  data: {
+    wpPage: { homeFields },
+  },
+}) => {
+  const image = getImage(homeFields.picture.localFile)
+
   return (
-    <main>
-      <Layout pageTitle="Welcome to Inghelbrecht Agency!">
-      <p>Lorem ipsum</p>
-      <StaticImage
-        alt="randomized unsplash image!"
-        src="../images/download.jpeg"
-      />
-      </Layout>
-    </main>
+    <Layout>
+      <section className={header}>
+        <article className={headerInfo}>
+          <h1 className={headerTitle}>{homeFields.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: homeFields.description,
+            }}
+          />
+          <a className={CTA} target="__blank" href={homeFields.callToAction.url}>
+            Book an artist
+          </a>
+        </article>
+        <div>
+          <GatsbyImage
+            image={image}
+            className={headerPicture}
+            alt={homeFields.picture.altText}
+          />
+        </div>
+      </section>
+      <section className={section}>
+      <h2 className={subtitle}>Featured Artists</h2>
+      <p>
+        // description
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed doo eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      </p>
+      <div className={artists}>
+        {homeFields.artists.map(artist => {
+        return <Artist slug={`artists/${artist.slug}`} key={artist.id} artist={artist} />
+      })}
+    </div>
+  </section>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    wpPage(slug: {eq: "home"}) {
+      homeFields {
+        artists {
+          ... on WpArtist {
+            id
+            slug
+            artistMeta {
+              artistName
+              firstName
+              lastName
+              profilePicture {
+                altText
+                localFile {
+                  childImageSharp {
+                    gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: true})
+                  }
+                }
+              }
+            }
+          }
+        }
+        callToAction {
+          title
+          url
+        }
+        title
+        description
+        picture {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
