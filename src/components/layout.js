b05da942c30880365dd005902d2797cb31e5db82
Silverback <jan.dausy@gmail.com>
2023-01-07 01:06:52 +0100

    footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@ import {
   navLinkText,
   siteTitle,
 } from "./layout.module.css"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -17,35 +18,50 @@ const Layout = ({ children }) => {
           title
         }
       }
+      wpPage(slug: { eq: "about-us" }) {
+        contactUsFields {
+            address
+            city
+            zipCode
+            facebook
+            instagram
+        }
+      }
     }
   `)
 
   return (
-    <div className={container}>
-      <title>{data.site.siteMetadata.title}</title>
-      <nav className={nav}>
-        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-        <ul className={navLinks}>
-          <li></li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/">
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/about">
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/artists">
-              Artists
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>{children}</main>
-    </div>
+    <>
+      <div className={container}>
+        <title>{data.site.siteMetadata.title}</title>
+        <nav className={nav}>
+          <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+          <ul className={navLinks}>
+            <li></li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/">
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/about">
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/artists">
+                Artists
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>{children}</main>
+      </div>
+      <Footer
+        siteTitle={data.site.siteMetadata.title}
+        companyInfo={data.wpPage.contactUsFields}
+      />
+    </>
   )
 }
 
